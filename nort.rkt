48bf5980b87abaec2a8246043c52c76ac70c8c6b
Stephen De Gabrielle <spdegabrielle@Stephens-MacBook-Pro.local>
2013-09-22 15:45:49 +0100

    updatign notes database

--- nort.rkt
+++ nort.rkt
@@ -5,24 +5,24 @@
 
 
 ;;Noter; 
-
-Query/title box
-- typing searches and displays a list of matching notes (matched by title/query)
-- on enter creates new or edits existing text note
- - what does enter do to the altenate matches in the list box?
-
-list box
-- list of matching items based on current query
-- clicking an item selects that as the current note
- - this does not change the query to alow switching between matching notes
-
-test box
-- text of currently selected item
-- saved on every keypress
-- ?undo
-- pressing enter gets a new line
-? tab moves back to search box
-? shift tab back to list box
+;
+;Query/title box
+;- typing searches and displays a list of matching notes (matched by title/query)
+;- on enter creates new or edits existing text note
+;- what does enter do to the altenate matches in the list box?
+;
+;list box
+;- list of matching items based on current query
+;- clicking an item selects that as the current note
+;- this does not change the query to alow switching between matching notes
+;
+;test box
+;- text of currently selected item
+;- saved on every keypress
+;- ?undo
+;- pressing enter gets a new line
+;? tab moves back to search box
+;? shift tab back to list box
 
 
 
@@ -40,116 +40,171 @@ test box
 (define (sorter a-list-of-strings)
   (sort a-list-of-strings string-ci<?))
 
-;; make-new-document: description -> void
-; makes a new editor (text%) with autowrap #t and modified keymap
-; updates the textdb global hash with the description as the key and the new %text as the value.
-; This should be in new class definition
-(define (make-new-document description)
-  (let* ; create new text object (note)
-      ((the-new-text (instantiate text% ()))
-       (the-keymap (send the-new-text get-keymap)))
-    (send the-new-text auto-wrap #t)
-    (keymap:setup-global the-keymap)
-    (send the-keymap add-function "Return to text-field-object" 
-          (lambda (text-field-object x) (send the-text-field focus)))
-    (send the-keymap map-function "c:q" "Return to text-field-object")
-    (hash-set! textdb description the-new-text)))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; 
-;; DEFINE THE DB - HASHTABLE USED  ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; 
-(define textdb (make-hasheq))
-;; key used is the description string
-;; value is a text% object
+(define notes-databse$ 
+  (class* object%  
+    (interface () new save load list) ;; 
+    (super-new)
+    ;; hash for testing
+    ;; move to sqlite
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; 
+    ;; DEFINE THE DB - HASHTABLE USED  ;;
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; 
+    (define textdb (make-hasheq))
+    ;; key used is the description string
+    ;; value is a text string
 
-;; matcher: description hashtable -> matching keys
-;; split description, find keys that match description 
-(define (matcher querystring)
-  (let ((terms (string-split querystring)); terms from the query
-        (list-of-keys (hash-map textdb (lambda (k v) k)))) 
-    ; sorted  list of documents that are the result of a logical and of search terms
-    (sorter (filter (lambda (k)
-                      (andmap (lambda (t) (string-ci=? t k))
-                              terms)) list-of-keys))))
-
-;;;;;;;;;;;;;;;
-;; callbacks ;;
-;;;;;;;;;;;;;;;   
-
-(define (the-text-field-callback text-field-object ce) ; ce:control-event-returned
-  (let ((query-or-name (send text-field-object get-value))
-        (this-event (send ce get-event-type)))
-    (cond
-      [(eq? this-event 'text-field-enter) ; create a new note or open existing one.
-       (let ; is it a new description?
-           ((current-text (hash-ref textdb query-or-name #f))) ; returns #f if a new document
-         (cond
-           [(eq? current-text #f) ;there is no matching document so pressing enter creates a new document
-            (let  ; (open)switch to matching text object(note)
-                ((editor (send the-editor-canvas get-editor current-text)))
-              (send the-editor-canvas focus)
-              (send editor erase)
-              (send the-list-box set (matcher query-or-name)) ; update list-box to show all matching keys
-              (send the-list-box append query-or-name); add description to list box
-              (send the-list-box set-string-selection query-or-name) ;and make current selection
-              )]
-           [else  ;; current-text contains an existing text note
-            (let ((editor (send the-editor-canvas get-editor current-text)))
-              (send the-editor-canvas focus)
-              (send editor erase)
-              (send editor insert current-text)
-              )
-            ;;
-            ]))]
-      [else ;; some other key was pressed
-       (begin
-         (send the-list-box set (matcher query-or-name)) ;;
-         ;; (printf "~a~n" description )
-         )])) ; not 'text-field-enter - search (interactive match) and auto-completion go here
-  1)
+    
+    ;; new : title text -> title
+    ;; create new entry in db
+    (define (new title text)
+          (hash-set! textdb title text)
+      title)
+    
+    
+    ;; save: title text -> title
+    ;; update entry for title
+    (define (save title text)
+      (hash-set! textdb title text) title)
+    
+    ;; load : title
+    (define (load title)
+      
+      text)
+    
+    
+    ;; return a list of titles on notes dabase
+    (define (list)
+      (hash-map textdb (lambda (k v) k)))
+    
+    
+    ))
 
 
-(define (the-list-box-callback list-box-object ce) ; ce:control-event-returned
-  (let ((this-event (send ce get-event-type)); always (eq? this-event 'list-box) 
-        (description (send list-box-object get-string-selection)))
-    (send the-text-field set-value description) 
-    (send the-editor-canvas set-editor (hash-ref textdb description #f))))
+
+
+
 
 ;;;;;;;;;;;;;;;;;;;;;
 ;; GUI definition  ;;
 ;;;;;;;;;;;;;;;;;;;;;
-(define editor (new (class text% 
-                      (init-field on-change-callback)
-                      (inherit get-text)
-                      (super-new)
-                      (define (on-change)
-                        (on-change-callback (get-text))
-                        )
-                      (augment on-change)
-                      )
-                    (on-change-callback (λ (text)void) )
-                    ))
-;;;;;;;;;;;;;;;;;;;;;
-;; Objects         ;;
-;;;;;;;;;;;;;;;;;;;;;
-(define notr-frame
-  (new frame%
-       (label "Noter - with incremental")
-       (width 600)
-       (height 600)))
-(send notr-frame show #t) 
 
-;; Add the 'description' text field 
-(define the-text-field (instantiate text-field% (#f notr-frame the-text-field-callback)))
-
-(define the-vertical-panel
-  (new panel:vertical-dragable%
-       (parent notr-frame)))
-;; add the matching descriptions list box
-(define the-list-box
-  (instantiate list-box% ("" '("") the-vertical-panel the-list-box-callback)
-    (stretchable-height #f)
-    (min-height 80)))
-(define the-editor-canvas (new editor-canvas%  
-                               (parent the-vertical-panel)
-                               [editor editor]))
+(define notr% 
+  (class object%
+    
+    (define notes (new notes-databse$))
+    
+    ;; matcher: description hashtable -> matching keys
+    ;; split description, find keys that match description 
+    (define (matcher querystring)
+      (let ((terms (string-split querystring)); terms from the query
+            (list-of-keys (send notes list))) 
+        ; sorted  list of documents that are the result of a logical and of search terms
+        (sorter (filter (lambda (k)
+                          (andmap (lambda (t) (string-ci=? t k))
+                                  terms)) list-of-keys))))
+    ;;;;;;;;;;;;;;;
+    ;; callbacks ;;
+    ;;;;;;;;;;;;;;;   
+    
+    ;; make-new-document: description -> void
+    ; makes a new editor (text%) with autowrap #t and modified keymap
+    ; updates the textdb global hash with the description as the key and the new %text as the value.
+    ; This should be in new class definition
+    (define (make-new-document description)
+      (let* ; create new text object (note)
+          (;(the-new-text (instantiate text% ()))
+           ;(the-keymap (send the-new-text get-keymap)))
+        ;(send the-new-text auto-wrap #t)
+        ;    (keymap:setup-global the-keymap)
+        ;    (send the-keymap add-function "Return to text-field-object" 
+        ;          (lambda (text-field-object x) (send the-text-field focus)))
+        ;    (send the-keymap map-function "c:q" "Return to text-field-object")
+        ))
+    
+    
+    
+    (define (the-list-box-callback list-box-object ce) ; ce:control-event-returned
+      (let ((this-event (send ce get-event-type)); always (eq? this-event 'list-box) 
+            (description (send list-box-object get-string-selection)))
+        (send the-text-field set-value description) 
+        (send the-editor-canvas set-editor (hash-ref textdb description #f))))
+    
+    (define (the-text-field-callback text-field-object ce) ; ce:control-event-returned
+      (let ((query-or-name (send text-field-object get-value))
+            (this-event (send ce get-event-type)))
+        (cond
+          [(eq? this-event 'text-field-enter) ; create a new note or open existing one.
+           (let ; is it a new description?
+               ((current-text (hash-ref textdb query-or-name #f))) ; returns #f if a new document
+             (cond
+               [(eq? current-text #f) ;there is no matching document so pressing enter creates a new document
+                (let  ; (open)switch to matching text object(note)
+                    ((editor (send the-editor-canvas get-editor current-text)))
+                  (send the-editor-canvas focus)
+                  (send editor erase)
+                  (send the-list-box set (matcher query-or-name)) ; update list-box to show all matching keys
+                  (send the-list-box append query-or-name); add description to list box
+                  (send the-list-box set-string-selection query-or-name) ;and make current selection
+                  )]
+               [else  ;; current-text contains an existing text note
+                (let ((editor (send the-editor-canvas get-editor current-text)))
+                  (send the-editor-canvas focus)
+                  (send editor erase)
+                  (send editor insert current-text)
+                  )
+                ;;
+                ]))]
+          [else ;; some other key was pressed
+           (begin
+             (send the-list-box set (matcher query-or-name)) ;;
+             ;; (printf "~a~n" description )
+             )])) ; not 'text-field-enter - search (interactive match) and auto-completion go here
+      1)
+    
+    
+    ;;;;;;;;;;
+    (super-new)
+    (define editor 
+      (new (class text% 
+             (init-field on-change-callback)
+             (inherit get-text)
+             (super-new)
+             (define (on-change)
+               (on-change-callback (get-text))
+               )
+             (augment on-change)
+             )
+           (on-change-callback (λ (text)void) )
+           ))
+    ;;;;;;;;;;;;;;;;;;;;;
+    ;; Objects         ;;
+    ;;;;;;;;;;;;;;;;;;;;;
+    (define notr-frame
+      (new frame%
+           (label "Noter - with incremental")
+           (width 600)
+           (height 600)))
+    (send notr-frame show #t) 
+    
+    ;; Add the 'description' text field 
+    (define the-text-field (new text-field% 
+                                (label #f) 
+                                (parent notr-frame)
+                                (the-text-field-callback )))
+    
+    (define the-vertical-panel
+      (new panel:vertical-dragable%
+           (parent notr-frame)))
+    ;; add the matching descriptions list box
+    (define the-list-box
+      (instantiate list-box% ("" '("") the-vertical-panel the-list-box-callback)
+        (stretchable-height #f)
+        (min-height 80)))
+    (define the-editor-canvas (new editor-canvas%  
+                                   (parent the-vertical-panel)
+                                   [editor editor]))
+    
+    
+    ))
+(new notr%)
